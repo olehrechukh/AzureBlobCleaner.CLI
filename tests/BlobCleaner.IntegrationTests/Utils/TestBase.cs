b48b448d01d4e2cdbd3be73da0b970fba6907f50
Oleh <oleh.hrechukh@eleks.com>
2022-09-04 02:11:04 +0200

    Add config

--- tests/BlobCleaner.IntegrationTests/Utils/TestBase.cs
+++ tests/BlobCleaner.IntegrationTests/Utils/TestBase.cs
@@ -5,15 +5,11 @@ namespace BlobCleaner.IntegrationTests.Utils;
 public class TestBase : IAsyncLifetime
 {
     protected readonly BlobClient Client;
-    protected readonly string ConnectionString;
 
-    protected TestBase()
-    {
-        ConnectionString = Environment.GetEnvironmentVariable(EnvironmentVariables.BlobConnectionString) ??
-                           throw new ArgumentNullException(nameof(EnvironmentVariables.BlobConnectionString));
+    protected const string ConnectionString =
+        "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
 
-        Client = new BlobClient(ConnectionString);
-    }
+    protected TestBase() => Client = new BlobClient(ConnectionString);
 
     public async Task InitializeAsync() => await Client.ClearAll();
 
